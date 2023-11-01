4d388e2802e8c0c8170723f3f7eedc3e53cfb6e9
manc1n1 <mancinij1111@gmail.com>
2023-10-31 22:34:52 -0400

    Add client folder

---
+++ client/src/js/database.js
@@ -0,0 +1,38 @@
+import { openDB } from 'idb';
+
+const initdb = async () =>
+	openDB('jate', 1, {
+		upgrade(db) {
+			if (db.objectStoreNames.contains('jate')) {
+				console.log('jate database already exists');
+				return;
+			}
+			db.createObjectStore('jate', {
+				keyPath: 'id',
+				autoIncrement: true,
+			});
+			console.log('jate database created');
+		},
+	});
+
+// TODO: Add logic to a method that accepts some content and adds it to the database
+export const putDb = async (content) => {
+	const jatedb = await openDB('jate', 1);
+	const text = jatedb.transaction('jate', 'readwrite');
+	const store = text.objectStore('jate');
+	const request = store.put({ id: 1, value: content });
+	const res = await request;
+	return res?.value;
+};
+
+// TODO: Add logic for a method that gets all the content from the database
+export const getDb = async () => {
+	const jatedb = await openDB('jate', 1);
+	const text = jatedb.transaction('jate', 'readonly');
+	const store = text.objectStore('jate');
+	const request = store.get(1);
+	const res = await request;
+	return res?.value;
+};
+
+initdb();
